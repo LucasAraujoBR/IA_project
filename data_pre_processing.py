cbd6e235a2ad5da575cb29490f2b824256341f07
Lucas Araújo <lucasaraujoti73@gmail.com>
2022-08-09 20:41:02 -0300

    Add pre-processing

---
+++ data_pre_processing.py
@@ -0,0 +1,32 @@
+import re
+
+
+def data_pre_processing(X):
+    lyrics = []
+    for sen in range(0, len(X)):
+        # Remove all the special characters
+        lyrics = re.sub(r'\W', ' ', str(X[sen]))
+        
+        # remove all single characters
+        lyrics = re.sub(r'\s+[a-zA-Z]\s+', ' ', lyrics)
+        
+        # Remove single characters from the start
+        lyrics = re.sub(r'\^[a-zA-Z]\s+', ' ', lyrics) 
+        
+        # Substituting multiple spaces with single space
+        lyrics = re.sub(r'\s+', ' ', lyrics, flags=re.I)
+        
+        # Removing prefixed 'b'
+        lyrics = re.sub(r'^b\s+', '', lyrics)
+        
+        # Converting to Lowercase
+        lyrics = lyrics.lower()
+        
+        # Lemmatization
+        lyrics = lyrics.split()
+
+        #lyrics = [stemmer.lemmatize(word) for word in lyrics]
+        lyrics_final = ' '.join(lyrics)
+        
+        lyrics.append(lyrics_final)
+        return lyrics
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from reading_txt import data_formated
-
+from data_pre_processing import data_pre_processing
 
 ##Return lis content lyrics
 #data_formated()
 print(len(data_formated()))
 
+print(data_pre_processing(data_formated()))
 
 
 
