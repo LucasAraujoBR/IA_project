801b8272c9689bc5be990cc373de7d11a4c69054
Lucas Araújo <lucasaraujoti73@gmail.com>
2022-08-10 20:43:02 -0300

    Filtro 1

--- genetics/operator.py
+++ genetics/operator.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta, abstractmethod
-
+import re
 
 class Operator(metaclass=ABCMeta):
     @abstractmethod
@@ -9,10 +9,35 @@ class Operator(metaclass=ABCMeta):
         """
     
     @abstractmethod
-    def function_name(self):
-        """
-        Retorna o nome da função
-        """
+    def character_filter(X):
+        lyrics = []
+        for sen in range(0, len(X)):
+            # Remove all the special characters
+            lyrics = re.sub(r'\W', ' ', str(X[sen]))
+            
+            # remove all single characters
+            lyrics = re.sub(r'\s+[a-zA-Z]\s+', ' ', lyrics)
+            
+            # Remove single characters from the start
+            lyrics = re.sub(r'\^[a-zA-Z]\s+', ' ', lyrics) 
+            
+            # Substituting multiple spaces with single space
+            lyrics = re.sub(r'\s+', ' ', lyrics, flags=re.I)
+            
+            # Removing prefixed 'b'
+            lyrics = re.sub(r'^b\s+', '', lyrics)
+            
+            # Converting to Lowercase
+            lyrics = lyrics.lower()
+            
+            # Lemmatization
+            lyrics = lyrics.split()
+
+            #lyrics = [stemmer.lemmatize(word) for word in lyrics]
+            lyrics_final = ' '.join(lyrics)
+            
+            lyrics.append(lyrics_final)
+        return lyrics
 
     @abstractmethod
     def function_python_definition(self):
